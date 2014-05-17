e93c23edb124883ddcbb81253834846df884592d
Otter <otc@otterthecat.com>
2014-05-17 10:24:21 -0500

    added server to app

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var p = require('./lib/paragraph');
 var randomNumber = require('./lib/randomNumber');
+var http = require('http');
+
 
 //console.log(p.generateParagraph(randomNumber(6,3)));
 
@@ -26,3 +28,16 @@ console.log("\n\n");
 
 p.build(2, bar(myString = ""));
 console.log(myString);
+
+var server = http.createServer().listen(1337);
+server.on('request', function(req, res){
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+
+	p.build(3, function(data){
+
+		res.write("<p>"+data+"</p>");
+	});
+
+	res.end();
+});
