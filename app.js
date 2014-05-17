2bbcdd3ad0e573c51d288750f58d5c1656460737
Otter <otc@otterthecat.com>
2014-05-17 10:00:25 -0500

    some refactoring to sentence & paragraph files and updated app.js implementation

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 var p = require('./lib/paragraph');
 var randomNumber = require('./lib/randomNumber');
 
-console.log(p.getParagraph(randomNumber(6,3)));
+//console.log(p.generateParagraph(randomNumber(6,3)));
+
+var foo = function(a){
+
+	var arr = a ? a : [];
+	return function(data){
+
+		arr.push(data);
+	};
+};
+
+var bar = function(){
+
+	return function(data){
+
+		myString += data;
+	};
+};
+
+p.build(4, foo(myArray = []));
+console.log(myArray);
+console.log("\n\n");
+
+p.build(2, bar(myString = ""));
+console.log(myString);
