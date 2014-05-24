7cf665b85eb198e1ae5bd6c362a82c509e000ff9
Otter <otc@otterthecat.com>
2014-05-24 09:14:10 -0500

    Renamed app.js to example.js and cleaned it up

--- example.js
+++ example.js
@@ -3,32 +3,12 @@ var words = require('./lib/words');
 var randomNumber = require('./lib/randomNumber');
 var http = require('http');
 
-var foo = function(a){
-
-	var arr = a ? a : [];
-	return function(data){
-
-		arr.push(data);
-	};
-};
-
-var returnArray = function(len){
-
-	ipsum.use(words.wilde).build(len, foo(a = []));
-	return a;
-};
 
 var server = http.createServer().listen(1337);
 server.on('request', function(req, res){
 
 	res.writeHead(200, {'Content-Type': 'text/html'});
 
-	// var jo = {
-	// 	title: ipsum.use(words.wilde).generateLine(),
-	// 	content: returnArray(3)
-	// };
-
-	// res.write(JSON.stringify(jo));
 	ipsum.use(words.wilde).build(4, function(data){
 
 		res.write("<p>" + data + "</p>");
